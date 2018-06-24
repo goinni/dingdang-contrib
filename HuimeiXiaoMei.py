7dd1419920c1696773cbe47fc1809afbef752e2f
jerry.hou <goinni@163.com>
2018-06-24 15:24:15 +0800

    updat

--- HuimeiXiaoMei.py
+++ HuimeiXiaoMei.py
@@ -4,12 +4,46 @@ import logging
 import sys
 import time
 import subprocess
+import json,urllib2
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 WORDS = ["XIAOMEI","JIAOMEI","LIUJIAO","AJIAO"]
 SLUG = "hmxiaomei"
+
+
+def getDiseaseEntity(words):
+   
+    entity = ""
+    try:
+        
+
+        textmod={"gender":"1","age":"18","ageType":"岁","symptom":words,"inquiryType":"0","hasRegimen":True,"v":1529818004373}
+        textmod = json.dumps(textmod)
+        print(textmod)
+        headers = {
+            'api-extend-params': '%7B%22hospitalGuid%22%3A4967%2C%22userGuid%22%3A2292613%2C%22doctorGuid%22%3A6394%2C%22serialNumber%22%3A2702113%2C%22authKey%22%3A%22C3B844493A477BCF3D7B73A5E902B269%22%2C%22i18n%22%3A%22cn%22%7D',
+            'Huimei_id': 'C3B844493A477BCF3D7B73A5E902B269',
+            "Content-Type": "application/json"
+        }
+        url='http://cdss.huimeionline.com/apollo/v_2_3/diagnose_through_interrogation'
+
+        req = urllib2.Request(url=url,data=textmod,headers=headers)
+        res = urllib2.urlopen(req)
+        res = res.read()
+
+        result = json.loads(res)
+        #诊断名
+        #diseaseName = result[u'body'][u'diseaseGroups'][0][u'diseases'][0][u'diseaseName']
+        #诊断说明
+        #definition = result[u'body'][u'diseaseGroups'][0][u'diseases'][0][u'definition']
+
+        entity = result[u'body'][u'diseaseGroups'][0][u'diseases'][0];
+
+    except :
+        pass
+    return entity
 
 def handle(text, mic, profile, wxbot=None):
     logger = logging.getLogger(__name__)
@@ -18,9 +52,12 @@ def handle(text, mic, profile, wxbot=None):
         input = mic.activeListen(MUSIC=True)
         if input is not None:
             mic.say('好哒，这就为您诊断', cache=True)
-            time.sleep(3)
-            mic.say('亲爱的，您好像是得了急性上呼吸道感染，可以吃点消炎类药，以上结果由惠每医疗提供，仅供参考', cache=True)
-            # subprocess.Popen("sudo reboot -f", shell=True)
+            time.sleep(1)
+            # 获取诊断信息
+            entity = getDiseaseEntity(input)
+
+            mic.say('亲爱的，您好像是得了'+entity[u'diseaseName']+'，'+ entity[u'definition']+'，以上结果由惠每医疗提供，仅供参考', cache=True)
+
             return
         mic.say('老娘没听清，再说一次呗', cache=True)
     except Exception, e:
@@ -29,3 +66,4 @@ def handle(text, mic, profile, wxbot=None):
 
 def isValid(text):
     return any(word in text for word in [u"小梅", u"娇妹", u"刘娇", u"阿娇"])
+
